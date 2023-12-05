a375ccd1af82c8e739f6c15af80fac9aee2d2f32
Chongchom <Pimma.dak99@gmail.com>
2023-12-05 19:25:49 +0700

    test

--- tu-warning/cypress/e2e/Login.cy.js
+++ tu-warning/cypress/e2e/Login.cy.js
@@ -46,4 +46,17 @@ describe('login', () => {
     cy.url().should('eq', 'http://localhost:3000/');
     
   })
+
+  it('login correct username and password', () => {
+    cy.visit('/');
+    cy.get('[data-test = "login-username"]').find('input').type('6409650055')
+    cy.get('[data-test = "login-password"]').find('input').type('Chom@020446')
+    cy.get('[data-test = "login-submit-button"]').click()
+    cy.get("h2#swal2-title.swal2-title").contains(/Success/)
+    cy.get("button.swal2-confirm.swal2-styled").click()
+    cy.get("h2#swal2-title.swal2-title").contains(/success/)
+    cy.get("button.swal2-confirm.swal2-styled").click()
+    cy.url().should('eq', 'http://localhost:3000/home');
+    
+  })
 })
